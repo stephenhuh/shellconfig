9c28c23fe03031eb1ef2439e9556e4686f1cd4b6
Stephen Huh <stephenhuh@gmail.com>
2015-01-07 08:52:02 -0600

    first commit of dotfiles

---
+++ shellconfig.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+##########################
+# shellconfig.sh
+# this script creates symlinks from home directory to any desired dotfiles in ~/dotfiles
+##########################
+
+
+######### Variables
+dir=~/dotfiles                                #dotfiles directory
+olddir=~/dotfiles_old                         #old dotfiles backup directory
+files="bashrc vimrc vim zshrc oh-my-zsh"      #list of files/folders to symlink in homedir
+
+########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles riectory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+#move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $files; do
+	echo "Moving any existing dotfiles from ~ to $olddir"
+	mv ~/.$file ~/dotfiles_old/
+	echo "Creating symlink to $file in home directory."
+	ln -s $dir/$file ~/.$file
+done
+
+
+
+
+
---
+++ vimrc
@@ -0,0 +1,22 @@
+set number
+set tabstop=3
+set shiftwidth=3
+set softtabstop=3
+set expandtab
+filetype on
+filetype plugin on
+filetype indent on
+syntax on
+set guifont=Menlo:h14
+colorscheme twilight
+set smartindent
+set autoindent
+set laststatus=2
+set linespace=3
+set wrap
+set textwidth=79
+set formatoptions=qrn1
+set incsearch
+set ignorecase
+set smartcase
+set mousehide
