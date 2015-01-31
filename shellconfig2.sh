4d6a8018ac4183c94b9a8eb9f42593f6c411342c
Stephen Huh <stephenhuh@gmail.com>
2015-01-31 01:20:36 -0600

    Changed tilde to $HOME

--- shellconfig2.sh
+++ shellconfig2.sh
@@ -12,35 +12,40 @@ files="vimrc vim"
 ######################################################################
 
 # create shellconfig_old inside homedir
-if [ -e "~/$olddir" ]
+echo "********************************************************"
+if [ -e "$HOME/$olddir" ]
     then echo "$olddir already exists!"
          echo "would you like to still create and overwrite $olddir? (y or n):"
          read decision
          if [ "$decision" -eq "y" ]
             then echo "you said yes"
             rm -rf ~/$olddir
+            echo "removed that dusty directory"
             mkdir -pv ~/$olddir #verbose and path creation
          elif [ "$decision" -eq "n" ]
             then echo "you said no"
+                 echo "********************************************************"
             exit
          else
             echo "invalid input"
             exit 1;
          fi
 fi
-echo "Creating the backup directory, $dir"
+echo "Creating the backup directory, $olddir"
 mkdir -pv ~/$olddir
 echo "Let's back up your files now..."
 for file in $files; do
-    if [ -e "~/.$file" ]
+    if [ -e "$HOME/.$file" ]
         then echo "Backing up .$file"
 	         mv ~/.$file ~/$olddir/$file
-	         echo "Now using new .$file\n"
+	         echo "Now using new .$file"
        	     mv ~/$dir/$file ~/.$file
 	else
 	     echo ".$file doesnt exist already, skipping backup!"
-         echo "Now using new .$file\n" 
-	     cp ~/$dir/$file ~/.$file
+         echo "Now using new .$file" 
+	     cp -r ~/$dir/$file ~/.$file
     fi
 done
 echo "enjoy your new shell"
+echo "remember to activate solarized in add-ons folder in .vim"
+echo "********************************************************"
