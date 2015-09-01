961ca95c699924f430ee6685492ca88a47f88ef2
Stephen Huh <stephenhuh@gmail.com>
2015-09-01 14:16:00 -0500

    add tmux and zshrc to install script

--- install.sh
+++ install.sh
@@ -4,14 +4,38 @@ echo ""
 echo "If you want to keep your .vimrc then back that shit up!"
 echo ""
 while true; do
-	read -p "Do you want to continue y or n? " yn
+	read -p "Do you want to install vimrc y or n? " yn
 	case $yn in
 		[Yy]* ) 
 			ln -sf ~/.vim/shellconfig/vimrc ~/.vimrc ;
 			git submodule update --init ;
 			vim +PluginInstall +qall
 			break;;
-		[Nn]* ) echo "Bye";
+		[Nn]* ) echo "Done with vimrc";
+			exit;;
+		* ) echo "Please answer yes or no.";
+			echo "";;
+	esac
+done
+while true; do
+	read -p "Do you want to install tmux.conf y or n? " yn
+	case $yn in
+		[Yy]* ) 
+			ln -sf ~/.vim/shellconfig/tmux.conf ~/.tmux.conf ;
+			break;;
+		[Nn]* ) echo "Done with tmux.conf";
+			exit;;
+		* ) echo "Please answer yes or no.";
+			echo "";;
+	esac
+done
+while true; do
+	read -p "Do you want to install zshrc y or n? " yn
+	case $yn in
+		[Yy]* ) 
+			ln -sf ~/.vim/shellconfig/zshrc ~/.zshrc ;
+			break;;
+		[Nn]* ) echo "Done with tmux.conf";
 			exit;;
 		* ) echo "Please answer yes or no.";
 			echo "";;
